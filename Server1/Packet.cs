a091a99b524ad9b1815f847b8f23eba48490db21
moto <mnxamoto@mail.ru>
2019-07-31 05:28:22 +0300

    - Переписал формат обмена данных на JSON - добавил в игнор .vs/Server1/v15/Server/sqlite3/db.lock (почему-то не индексируется)

---
+++ Server1/Packet.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Server1
+{
+    class Packet
+    {
+        public string commanda;
+        public byte[] data;
+    }
+
+    class FileInfoKratko
+    {
+        public string name;
+        public long size;
+    }
+}
--- Server1/Program.cs
+++ Server1/Program.cs
@@ -5,41 +5,40 @@ using System.Net.Sockets;
 using System.Linq;
 using System.Threading;
 using System.IO;
-//using System.Diagnostics;
 
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Timers;
 using System.Media;
+using Server1;
+using Newtonsoft.Json;
 
 namespace SocketServer
 {
     class Program
     {
         static Socket sListener;
-        static System.Collections.Generic.List<Socket> clients = new System.Collections.Generic.List<Socket>();
+        static List<Socket> clients = new List<Socket>();
 
         static Thread Thread1;
 
         static string history = "";
-        static System.Collections.Generic.List<String> logins = new System.Collections.Generic.List<string>();
-
-        static byte[] ping = new byte[1] { 0 }; //Синхронизация
+        static List<String> logins = new List<string>();
 
         static void Main(string[] args)
         {
-            Console.Write("[" + DateTime.Now.ToString("HH:mm:ss") + "] Лог сервера:\r\n");
 
             Directory.CreateDirectory(Directory.GetCurrentDirectory() + "\\Логи");
             Directory.CreateDirectory(Directory.GetCurrentDirectory() + "\\Выгрузки");
             File.AppendAllText(Directory.GetCurrentDirectory() + "\\Логи\\" + DateTime.Now.ToString("dd.MM.yyyy") + ".txt", "[" + DateTime.Now.ToString("HH:mm:ss") + "] Лог сервера:\r\n");
-            
+
             // Устанавливаем для сокета локальную конечную точку
-            IPHostEntry ipHost = Dns.GetHostEntry("192.168.0.101");
-            //IPHostEntry ipHost = Dns.GetHostEntry("127.0.0.1");
+            IPHostEntry ipHost = getIpHost();
             IPAddress ipAddr = ipHost.AddressList.FirstOrDefault((a) => a.AddressFamily == AddressFamily.InterNetwork);
             IPEndPoint ipEndPoint = new IPEndPoint(ipAddr, 1001);
+
+            Console.Write("[" + DateTime.Now.ToString("HH:mm:ss") + "] Лог сервера:\r\n");
 
             // Создаем сокет Tcp/Ip
             sListener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
@@ -80,14 +79,8 @@ namespace SocketServer
         {
             while (true)
             {
-                string data = null;
-                byte[] bytes = new byte[1024];
-                int bytesRec = 0;
-                //Console.WriteLine("\r\nОжидаем соединение через порт {0}", ipEndPoint);
-
                 for (int i = 0; i < clients.Count; i++)
                 {
-
                     if (!clients[i].Poll(100, SelectMode.SelectRead))
                     {
                         continue;
@@ -95,9 +88,144 @@ namespace SocketServer
 
                     try
                     {
-                        bytesRec = clients[i].Receive(bytes);
-                        // Показываем данные на консоли
-                        data = Encoding.UTF8.GetString(bytes, 0, bytesRec);
+                        string message;
+                        string fileName;
+
+                        Packet packet = GetPacket(clients[i]);
+
+                        switch (packet.commanda)
+                        {
+                            case "Сообщение":
+                                message = Encoding.UTF8.GetString(packet.data);
+                                message = "[" + DateTime.Now.ToString("HH:mm:ss") + "] " + logins[i] + " : " + message;
+                                Send("Сообщение", message);
+                                break;
+                            case "Подключиться":
+                                logins.Add(Encoding.UTF8.GetString(packet.data));
+                                message = "К чату подключился: " + logins[i] + " [" + clients[i].RemoteEndPoint + "]\r\n";
+                                Send("Сообщение", message);
+                                Thread.Sleep(1000);
+                                ObnovlenieInfo(clients[i]);
+                                break;
+                            case "Отключиться":
+                                message = "От чата отключился: " + logins[i] + "\r\n";
+                                Send("Сообщение", message);
+                                logins.RemoveAt(i);
+                                ObnovlenieInfo(clients[i]);
+                                break;
+                            case "Синхронизация":
+                                //Тупо синхронизация
+                                break;
+                            case "Файлы":
+                                //Походу не надо
+                                break;
+                            case "История":
+                                Send("Сообщение", history, clients[i]);
+                                break;
+                            case "Выгрузить":
+                                {
+                                    fileName = Encoding.UTF8.GetString(packet.data);
+
+                                    Send("Синхронизация", clients[i]);
+
+                                    FileStream stream = new FileStream(Directory.GetCurrentDirectory() + "\\Выгрузки\\" + fileName, FileMode.Create, FileAccess.Write);
+                                    BinaryWriter f = new BinaryWriter(stream);
+                                    byte[] buffer = new byte[8192]; //Буфер для файла
+                                    byte[] bFSize = new byte[512]; //Размер файла
+
+                                    int bytesiRec = clients[i].Receive(bFSize); //Принимаем размер
+                                    Send("Синхронизация", clients[i]);
+                                    int fSize = Convert.ToInt32(Encoding.UTF8.GetString(bFSize, 0, bytesiRec));
+
+                                    int processed = 0; //Байт принято
+                                    while (processed < fSize) //Принимаем файл
+                                    {
+                                        if ((fSize - processed) < 8192)
+                                        {
+                                            int bytesi = (fSize - processed);
+                                            byte[] buf = new byte[bytesi];
+                                            bytesi = clients[i].Receive(buf);
+                                            f.Write(buf, 0, bytesi);
+                                        }
+                                        else
+                                        {
+                                            int bytesi = clients[i].Receive(buffer);
+                                            f.Write(buffer, 0, bytesi);
+                                        }
+                                        Send("Синхронизация", clients[i]);
+                                        processed += 8192;
+                                    }
+                                    f.Close();
+                                    stream.Close();
+
+                                    Thread.Sleep(1000);
+                                    message = logins[i] + " выгрузил на сервер файл: " + fileName + "\r\n";
+                                    Send("Сообщение", message);
+                                    Thread.Sleep(1000);
+                                    /*
+                                    Send("!логины" + loginsMsg() + "\r\n");
+                                    clients[i].Receive(ping);
+                                    */
+                                    Send("Файлы", fileMsgStringArray());
+                                }
+                                break;
+                            case "Загрузить":
+                                {
+                                    fileName = Directory.GetCurrentDirectory() + "\\Выгрузки\\" + Encoding.UTF8.GetString(packet.data);
+
+                                    byte[] ping = new byte[1] { 0 }; //Синхронизация
+                                    Send("Загрузить", Path.GetFileName(fileName), clients[i]);
+
+                                    if (Sinhronizatiya(clients[i]))
+                                    {
+                                        return;
+                                    }
+
+                                    FileStream stream = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+                                    BinaryReader f = new BinaryReader(stream);
+                                    byte[] buffer = new byte[8192]; //Буфер для файла
+                                    int bytesi = 8192;
+                                    int fSize = Convert.ToInt32(stream.Length);
+
+                                    byte[] bFSize = Encoding.UTF8.GetBytes(Convert.ToString(fSize)); //Размер файла
+                                    clients[i].Send(bFSize); //Передаем размер
+                                    if (Sinhronizatiya(clients[i]))
+                                    {
+                                        return;
+                                    }
+
+                                    int processed = 0; //Байт передано
+                                    while (processed < fSize) //Передаем файл
+                                    {
+                                        if ((fSize - processed) < 8192)
+                                        {
+                                            bytesi = Convert.ToInt32(fSize - processed);
+                                            byte[] buf = new byte[bytesi];
+                                            f.Read(buf, 0, bytesi);
+                                            clients[i].Send(buf);
+                                        }
+                                        else
+                                        {
+                                            f.Read(buffer, 0, bytesi);
+                                            clients[i].Send(buffer);
+                                        }
+
+                                        if (Sinhronizatiya(clients[i]))
+                                        {
+                                            return;
+                                        }
+                                        processed += 8192;
+                                    }
+                                    f.Close();
+                                    stream.Close();
+                                }
+                                break;
+                            default:
+                                break;
+                        }
+
+
+                        /*
                         switch (data)
                         {
                             case "!история\r\n":
@@ -213,12 +341,17 @@ namespace SocketServer
                                         else
                                         {
                                             // Отправляем ответ клиентам
-                                            Send("[" + DateTime.Now.ToString("HH:mm:ss") + "] " + logins[i] + " : " + data);
+                                            Packet packet = JsonConvert.DeserializeObject<Packet>(data);
+                                            string message = Encoding.UTF8.GetString(packet.data);
+                                            Send("[" + DateTime.Now.ToString("HH:mm:ss") + "] " + logins[i] + " : " + message);
+                                            
+                                            //Send("[" + DateTime.Now.ToString("HH:mm:ss") + "] " + logins[i] + " : " + data);
                                         }
                                     }
                                 }
                                 break;
                         }
+                        */
                     }
                     catch (Exception ex)
                     {
@@ -230,49 +363,141 @@ namespace SocketServer
             }
         }
 
-        static string loginsMsg()
+        static string[] loginsMsgStringArray()
         {
-            string text = "|";
+            string[] result = new string[logins.Count];
+
             for (int k = 0; k < logins.Count; k++)
             {
-                text += logins[k] + "|";
+                result[k] = logins[k];
             }
-            return text;
+            return result;
         }
 
-        static string fileMsg()
+        static FileInfoKratko[] fileMsgStringArray()
         {
-            string text = "|";
             string[] files = Directory.GetFiles(Directory.GetCurrentDirectory() + "\\Выгрузки");
-            FileInfo fileInfos;
+
+            FileInfoKratko[] result = new FileInfoKratko[files.Length];
+
             for (int k = 0; k < files.Length; k++)
             {
-                fileInfos = new FileInfo(files[k]);
-                text += Path.GetFileName(files[k]) + "|" + fileInfos.Length + "|";
+                FileInfo fileInfo = new FileInfo(files[k]);
+                result[k] = new FileInfoKratko();
+                result[k].name = fileInfo.Name;
+                result[k].size = fileInfo.Length;
             }
-            return text;
+
+            return result;
         }
 
-        static void Send(string message)
+        static void ObnovlenieInfo(Socket client)
         {
-            byte[] msg = Encoding.UTF8.GetBytes(message);
+            Send("Логины", loginsMsgStringArray());
 
-            Console.Write(message);
-            File.AppendAllText(Directory.GetCurrentDirectory() + "\\Логи\\" + DateTime.Now.ToString("dd.MM.yyyy") + ".txt", message);
-            history += message;
+            if (Sinhronizatiya(client))
+            {
+                Send("Файлы", fileMsgStringArray(), client);
+            }
+        }
+
+        static bool Sinhronizatiya(Socket client)
+        {
+            if (GetPacket(client).commanda == "Синхронизация")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        static Packet GetPacket(Socket client)
+        {
+            byte[] bytes = new byte[1024];
+            int bytesRec = client.Receive(bytes);
+            string data = Encoding.UTF8.GetString(bytes, 0, bytesRec);
+            Packet packet = JsonConvert.DeserializeObject<Packet>(data);
+
+            return packet;
+        }
+
+        static void Send(string commanda, object messageObject)
+        {
+            string messageString = JsonConvert.SerializeObject(messageObject);
+            byte[] messageBytes = Encoding.UTF8.GetBytes(messageString);
+
+            Console.WriteLine(messageString);
+            File.AppendAllText(Directory.GetCurrentDirectory() + "\\Логи\\" + DateTime.Now.ToString("dd.MM.yyyy") + ".txt", messageString + "\r\n");
+            history += messageString;
+
+            Packet packet = new Packet();
+            packet.commanda = commanda;
+            packet.data = messageBytes;
+
+            string packetString = JsonConvert.SerializeObject(packet);
+            byte[] packetBytes = Encoding.UTF8.GetBytes(packetString);
 
             for (int k = 0; k < clients.Count; k++)
             {
-                try
-                {
-                    clients[k].Send(msg);
-                }
-                catch (Exception ex)
-                {
-                    Console.Write(ex.Message + "\r\n" + ex.StackTrace + "\r\n");
-                    File.AppendAllText(Directory.GetCurrentDirectory() + "\\Логи\\" + DateTime.Now.ToString("dd.MM.yyyy") + ".txt", ex.Message + "\r\n" + ex.StackTrace + "\r\n");
-                }
+                clients[k].Send(packetBytes);
             }
         }
+
+        static void Send(string commanda, Socket client)
+        {
+            Packet packet = new Packet();
+            packet.commanda = commanda;
+
+            string packetString = JsonConvert.SerializeObject(packet);
+            byte[] packetBytes = Encoding.UTF8.GetBytes(packetString);
+
+            client.Send(packetBytes);
+        }
+
+        static void Send(string commanda, object messageObject, Socket client)
+        {
+            string messageString = JsonConvert.SerializeObject(messageObject);
+            byte[] messageBytes = Encoding.UTF8.GetBytes(messageString);
+
+            Packet packet = new Packet();
+            packet.commanda = commanda;
+            packet.data = messageBytes;
+
+            string packetString = JsonConvert.SerializeObject(packet);
+            byte[] packetBytes = Encoding.UTF8.GetBytes(packetString);
+
+            client.Send(packetBytes);
+        }
+
+        static IPHostEntry getIpHost()
+        {
+            Console.Write(
+                "Выберите локальную конечную точку для сокета:\r\n" +
+                "1. 127.0.0.1\r\n" +
+                "2. 192.168.0.101\r\n" +
+                "или введите вручную\r\n");
+
+            string ipHostString = Console.ReadLine();
+            IPHostEntry ipHost = new IPHostEntry();
+
+            switch (ipHostString)
+            {
+                case "1":
+                    ipHost = Dns.GetHostEntry("127.0.0.1");
+                    break;
+                case "2":
+                    ipHost = Dns.GetHostEntry("192.168.0.101");
+                    break;
+                default:
+                    ipHost = Dns.GetHostEntry(ipHostString);
+                    break;
+            }
+
+            return ipHost;
+        }
+
+
     }
 }
