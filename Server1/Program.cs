983877de27dec710ee24b80e6efee16307b45ce5
moto <mnxamoto@mail.ru>
2019-07-31 05:31:32 +0300

    Убрал нерабочий код

--- Server1/Program.cs
+++ Server1/Program.cs
@@ -6,11 +6,7 @@ using System.Linq;
 using System.Threading;
 using System.IO;
 
-using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Timers;
-using System.Media;
 using Server1;
 using Newtonsoft.Json;
 
@@ -223,135 +219,6 @@ namespace SocketServer
                             default:
                                 break;
                         }
-
-
-                        /*
-                        switch (data)
-                        {
-                            case "!история\r\n":
-                                byte[] msg = Encoding.UTF8.GetBytes(history);
-                                clients[i].Send(msg);
-                                break;
-                            case "!отключиться":
-                                //handlers[i].Disconnect(false);
-                                Send("От чата отключился: " + logins[i] + "\r\n");
-                                logins.RemoveAt(i);
-                                Send("!логины" + loginsMsg() + "\r\n");
-                                clients[i].Receive(ping);
-                                Send("!файлы" + fileMsg() + "\r\n");
-                                break;
-                            default:
-                                string log;
-                                if ((data.Length > 13) && (data.Substring(0, 13) == "!подключиться"))
-                                {
-                                    logins.Add(data.Substring(13, data.Length - 13));
-                                    log = "К чату подключился: " + logins[i] + " [" + clients[i].RemoteEndPoint + "]\r\n";
-                                    Send(log);
-                                    Thread.Sleep(1000);
-                                    Send("!логины" + loginsMsg() + "\r\n");
-                                    clients[i].Receive(ping);
-                                    Send("!файлы" + fileMsg() + "\r\n");
-                                }
-                                else
-                                {
-                                    if ((data.Length > 10) && (data.Substring(0, 10) == "!выгрузить"))
-                                    {
-                                        string fileName = data.Substring(10, data.Length - 10);
-                                        Send(logins[i] + " выгрузил на сервер файл: " + fileName + "\r\n");
-
-                                        clients[i].Send(ping); // 1
-
-                                        FileStream stream = new FileStream(Directory.GetCurrentDirectory() + "\\Выгрузки\\" + fileName, FileMode.Create, FileAccess.Write);
-                                        BinaryWriter f = new BinaryWriter(stream);
-                                        byte[] buffer = new byte[8192]; //Буфер для файла
-                                        byte[] bFSize = new byte[512]; //Размер файла
-
-                                        int bytesiRec = clients[i].Receive(bFSize); //Принимаем размер
-                                        clients[i].Send(ping); // 2
-                                        int fSize = Convert.ToInt32(Encoding.UTF8.GetString(bFSize, 0, bytesiRec));
-
-                                        int processed = 0; //Байт принято
-                                        while (processed < fSize) //Принимаем файл
-                                        {
-                                            if ((fSize - processed) < 8192)
-                                            {
-                                                int bytesi = (fSize - processed);
-                                                byte[] buf = new byte[bytesi];
-                                                bytesi = clients[i].Receive(buf);
-                                                f.Write(buf, 0, bytesi);
-                                            }
-                                            else
-                                            {
-                                                int bytesi = clients[i].Receive(buffer);
-                                                f.Write(buffer, 0, bytesi);
-                                            }
-                                            clients[i].Send(ping); // 3
-                                            processed += 8192;
-                                        }
-                                        f.Close();
-                                        stream.Close();
-                                        Thread.Sleep(1000);
-                                        Send("!логины" + loginsMsg() + "\r\n");
-                                        clients[i].Receive(ping);
-                                        Send("!файлы" + fileMsg() + "\r\n");
-                                    }
-                                    else
-                                    {
-                                        if ((data.Length > 10) && (data.Substring(0, 10) == "!загрузить"))
-                                        {
-                                            string fileName = Directory.GetCurrentDirectory() + "\\Выгрузки\\" + data.Substring(10, data.Length - 12);
-
-                                            byte[] ping = new byte[1] { 0 }; //Синхронизация
-                                            byte[] message = Encoding.UTF8.GetBytes("!загрузить" + Path.GetFileName(fileName));
-                                            int bytesSent = clients[i].Send(message);
-                                            clients[i].Receive(ping); // 1
-
-                                            FileStream stream = new FileStream(fileName, FileMode.Open, FileAccess.Read);
-                                            BinaryReader f = new BinaryReader(stream);
-                                            byte[] buffer = new byte[8192]; //Буфер для файла
-                                            int bytesi = 8192;
-                                            int fSize = Convert.ToInt32(stream.Length);
-
-                                            byte[] bFSize = Encoding.UTF8.GetBytes(Convert.ToString(fSize)); //Размер файла
-                                            clients[i].Send(bFSize); //Передаем размер
-                                            clients[i].Receive(ping); // 2
-
-                                            int processed = 0; //Байт передано
-                                            while (processed < fSize) //Передаем файл
-                                            {
-                                                if ((fSize - processed) < 8192)
-                                                {
-                                                    bytesi = Convert.ToInt32(fSize - processed);
-                                                    byte[] buf = new byte[bytesi];
-                                                    f.Read(buf, 0, bytesi);
-                                                    clients[i].Send(buf);
-                                                }
-                                                else
-                                                {
-                                                    f.Read(buffer, 0, bytesi);
-                                                    clients[i].Send(buffer);
-                                                }
-
-                                                clients[i].Receive(ping); // 3
-                                                processed += 8192;
-                                            }
-                                            f.Close();
-                                            stream.Close();
-                                        }
-                                        else
-                                        {
-                                            // Отправляем ответ клиентам
-                                            Packet packet = JsonConvert.DeserializeObject<Packet>(data);
-                                            string message = Encoding.UTF8.GetString(packet.data);
-                                            Send("[" + DateTime.Now.ToString("HH:mm:ss") + "] " + logins[i] + " : " + message);
-                                            
-                                            //Send("[" + DateTime.Now.ToString("HH:mm:ss") + "] " + logins[i] + " : " + data);
-                                        }
-                                    }
-                                }
-                                break;
-                        }
-                        */
                     }
                     catch (Exception ex)
                     {
